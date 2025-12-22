5112f957344fea0988a5fc91fbbbbd3fefb4dedb
Christian Bremer <bremersee@googlemail.com>
2025-12-22 13:28:48 +0100

    jbox init

---
+++ spring-integration-parent/spring-integration-core/src/main/java/org/bremersee/spring/core/regex/RegexFlags.java
@@ -0,0 +1,128 @@
+package org.bremersee.spring.core.regex;
+
+import static java.util.Objects.isNull;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Optional;
+import java.util.regex.Pattern;
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * The regex flags.
+ */
+@Data
+public class RegexFlags implements Serializable {
+
+  @Serial
+  private static final long serialVersionUID = 1L;
+
+  private boolean unixLines;
+
+  private boolean caseInsensitive;
+
+  private boolean comments;
+
+  private boolean multiLine;
+
+  private boolean literal;
+
+  private boolean dotAll;
+
+  private boolean unicodeCase;
+
+  private boolean canonEq;
+
+  private boolean unicodeCharacterClass;
+
+  /**
+   * Instantiates new regex flags.
+   */
+  public RegexFlags() {
+    super();
+  }
+
+  /**
+   * Instantiates new regex flags.
+   *
+   * @param unixLines the unix lines flag
+   * @param caseInsensitive the case-insensitive flag
+   * @param comments the comments flag
+   * @param multiLine the multi line flag
+   * @param literal the literal flag
+   * @param dotAll the dot all flag
+   * @param unicodeCase the unicode-case flag
+   * @param canonEq the canon eq flag
+   * @param unicodeCharacterClass the unicode-character class flag
+   */
+  @Builder
+  RegexFlags(boolean unixLines, boolean caseInsensitive, boolean comments, boolean multiLine,
+      boolean literal, boolean dotAll, boolean unicodeCase, boolean canonEq,
+      boolean unicodeCharacterClass) {
+    this.unixLines = unixLines;
+    this.caseInsensitive = caseInsensitive;
+    this.comments = comments;
+    this.multiLine = multiLine;
+    this.literal = literal;
+    this.dotAll = dotAll;
+    this.unicodeCase = unicodeCase;
+    this.canonEq = canonEq;
+    this.unicodeCharacterClass = unicodeCharacterClass;
+  }
+
+  /**
+   * To regex flags.
+   *
+   * @return the optional
+   */
+  public Optional<Integer> toRegexFlags() {
+    int flags = 0;
+    if (isUnixLines()) {
+      flags = Pattern.UNIX_LINES;
+    }
+    if (isCaseInsensitive()) {
+      flags = flags | Pattern.CASE_INSENSITIVE;
+    }
+    if (isComments()) {
+      flags = flags | Pattern.COMMENTS;
+    }
+    if (isMultiLine()) {
+      flags = flags | Pattern.MULTILINE;
+    }
+    if (isLiteral()) {
+      flags = flags | Pattern.LITERAL;
+    }
+    if (isDotAll()) {
+      flags = flags | Pattern.DOTALL;
+    }
+    if (isUnicodeCase()) {
+      flags = flags | Pattern.UNICODE_CASE;
+    }
+    if (isCanonEq()) {
+      flags = flags | Pattern.CANON_EQ;
+    }
+    if (isUnicodeCharacterClass()) {
+      flags = flags | Pattern.UNICODE_CHARACTER_CLASS;
+    }
+    return Optional.of(flags)
+        .filter(v -> v > 0);
+  }
+
+  /**
+   * Compile pattern.
+   *
+   * @param regex the regex
+   * @return the pattern
+   */
+  @SuppressWarnings("MagicConstant")
+  public Pattern compile(String regex) {
+    if (isNull(regex)) {
+      return null;
+    }
+    return toRegexFlags()
+        .map(flags -> Pattern.compile(regex, flags))
+        .orElseGet(() -> Pattern.compile(regex));
+  }
+
+}
---
+++ spring-integration-parent/spring-integration-core/src/test/java/org/bremersee/spring/core/regex/RegexFlagsTest.java
@@ -0,0 +1,206 @@
+package org.bremersee.spring.core.regex;
+
+import java.util.regex.Pattern;
+import org.assertj.core.api.SoftAssertions;
+import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+/**
+ * The regex flags test.
+ */
+@ExtendWith(SoftAssertionsExtension.class)
+class RegexFlagsTest {
+
+  /**
+   * Email regex from <a href="https://emailregex.com/">emailregex.com</a> (RFC 5322 Official
+   * Standard).
+   *
+   * <p>RFC 6530 is not supported.
+   */
+  private static final String EMAIL_REGEX = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+"
+      + "(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*"
+      + "|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]"
+      + "|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+"
+      + "[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}"
+      + "(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:"
+      + "(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]"
+      + "|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])";
+
+  /**
+   * To regex flags.
+   *
+   * @param softly the softly
+   */
+  @Test
+  void toRegexFlags(SoftAssertions softly) {
+    RegexFlags none = new RegexFlags();
+    softly.assertThat(none.toRegexFlags()).isEmpty();
+    softly.assertThat(none.toString()).doesNotContain("true");
+
+    RegexFlags some = RegexFlags.builder()
+        .caseInsensitive(true)
+        .unicodeCase(true)
+        .build();
+    softly.assertThat(some.toRegexFlags())
+        .hasValue(Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
+    softly.assertThat(some.toString()).contains("true");
+
+    RegexFlags all = new RegexFlags(
+        true,
+        true,
+        true,
+        true,
+        true,
+        true,
+        true,
+        true,
+        true);
+    softly.assertThat(all.toRegexFlags())
+        .hasValue(Pattern.UNIX_LINES
+            | Pattern.CASE_INSENSITIVE
+            | Pattern.COMMENTS
+            | Pattern.MULTILINE
+            | Pattern.LITERAL
+            | Pattern.DOTALL
+            | Pattern.UNICODE_CASE
+            | Pattern.CANON_EQ
+            | Pattern.UNICODE_CHARACTER_CLASS);
+  }
+
+  /**
+   * Compile.
+   *
+   * @param softly the softly
+   */
+  @Test
+  void compile(SoftAssertions softly) {
+    Pattern caseInsensitivePattern = RegexFlags.builder().caseInsensitive(true).build()
+        .compile(EMAIL_REGEX);
+    softly.assertThat(caseInsensitivePattern.matcher("FOO@example.com").matches()).isTrue();
+
+    Pattern pattern = new RegexFlags().compile(EMAIL_REGEX);
+    softly.assertThat(pattern.matcher("FOO@example.com").matches()).isFalse();
+
+    Pattern empty = new RegexFlags().compile(null);
+    softly.assertThat(empty).isNull();
+  }
+
+  /**
+   * Is unix lines.
+   *
+   * @param softly the softly
+   */
+  @Test
+  void isUnixLines(SoftAssertions softly) {
+    RegexFlags flags = new RegexFlags();
+    flags.setUnixLines(true);
+    softly.assertThat(flags.toRegexFlags()).hasValue(Pattern.UNIX_LINES);
+    softly.assertThat(flags).isEqualTo(RegexFlags.builder().unixLines(true).build());
+  }
+
+  /**
+   * Is case-insensitive.
+   *
+   * @param softly the softly
+   */
+  @Test
+  void isCaseInsensitive(SoftAssertions softly) {
+    RegexFlags flags = new RegexFlags();
+    flags.setCaseInsensitive(true);
+    softly.assertThat(flags.toRegexFlags()).hasValue(Pattern.CASE_INSENSITIVE);
+    softly.assertThat(flags).isEqualTo(RegexFlags.builder().caseInsensitive(true).build());
+  }
+
+  /**
+   * Is comments.
+   *
+   * @param softly the softly
+   */
+  @Test
+  void isComments(SoftAssertions softly) {
+    RegexFlags flags = new RegexFlags();
+    flags.setComments(true);
+    softly.assertThat(flags.toRegexFlags()).hasValue(Pattern.COMMENTS);
+    softly.assertThat(flags).isEqualTo(RegexFlags.builder().comments(true).build());
+  }
+
+  /**
+   * Is multi line.
+   *
+   * @param softly the softly
+   */
+  @Test
+  void isMultiLine(SoftAssertions softly) {
+    RegexFlags flags = new RegexFlags();
+    flags.setMultiLine(true);
+    softly.assertThat(flags.toRegexFlags()).hasValue(Pattern.MULTILINE);
+    softly.assertThat(flags).isEqualTo(RegexFlags.builder().multiLine(true).build());
+  }
+
+  /**
+   * Is literal.
+   *
+   * @param softly the softly
+   */
+  @Test
+  void isLiteral(SoftAssertions softly) {
+    RegexFlags flags = new RegexFlags();
+    flags.setLiteral(true);
+    softly.assertThat(flags.toRegexFlags()).hasValue(Pattern.LITERAL);
+    softly.assertThat(flags).isEqualTo(RegexFlags.builder().literal(true).build());
+  }
+
+  /**
+   * Is dot all.
+   *
+   * @param softly the softly
+   */
+  @Test
+  void isDotAll(SoftAssertions softly) {
+    RegexFlags flags = new RegexFlags();
+    flags.setDotAll(true);
+    softly.assertThat(flags.toRegexFlags()).hasValue(Pattern.DOTALL);
+    softly.assertThat(flags).isEqualTo(RegexFlags.builder().dotAll(true).build());
+  }
+
+  /**
+   * Is unicode-case.
+   *
+   * @param softly the softly
+   */
+  @Test
+  void isUnicodeCase(SoftAssertions softly) {
+    RegexFlags flags = new RegexFlags();
+    flags.setUnicodeCase(true);
+    softly.assertThat(flags.toRegexFlags()).hasValue(Pattern.UNICODE_CASE);
+    softly.assertThat(flags).isEqualTo(RegexFlags.builder().unicodeCase(true).build());
+  }
+
+  /**
+   * Is canon eq.
+   *
+   * @param softly the softly
+   */
+  @Test
+  void isCanonEq(SoftAssertions softly) {
+    RegexFlags flags = new RegexFlags();
+    flags.setCanonEq(true);
+    softly.assertThat(flags.toRegexFlags()).hasValue(Pattern.CANON_EQ);
+    softly.assertThat(flags).isEqualTo(RegexFlags.builder().canonEq(true).build());
+  }
+
+  /**
+   * Is unicode-character class.
+   *
+   * @param softly the softly
+   */
+  @Test
+  void isUnicodeCharacterClass(SoftAssertions softly) {
+    RegexFlags flags = new RegexFlags();
+    flags.setUnicodeCharacterClass(true);
+    softly.assertThat(flags.toRegexFlags()).hasValue(Pattern.UNICODE_CHARACTER_CLASS);
+    softly.assertThat(flags).isEqualTo(RegexFlags.builder().unicodeCharacterClass(true).build());
+  }
+
+}
