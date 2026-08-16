a03bdaa85c3837f5f141ffcd84b264b95c52f4f8
Christian Bremer <bremersee@googlemail.com>
2026-08-16 16:20:54 +0200

    Version 6.0.2

--- spring-integration-parent/spring-integration-boot-autoconfigure/src/main/java/org/bremersee/spring/boot/autoconfigure/web/reactive/CorsConfigurationSourceAutoConfiguration.java
+++ spring-integration-parent/spring-integration-boot-autoconfigure/src/main/java/org/bremersee/spring/boot/autoconfigure/web/reactive/CorsConfigurationSourceAutoConfiguration.java
@@ -21,6 +21,7 @@ import org.apache.commons.logging.LogFactory;
 import org.bremersee.spring.boot.autoconfigure.web.CorsProperties;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication.Type;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
@@ -40,6 +41,7 @@ import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 @ConditionalOnClass(name = {
     "org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource"
 })
+@ConditionalOnProperty(prefix = "bremersee.web.cors", name = "enabled", havingValue = "true")
 @EnableConfigurationProperties(CorsProperties.class)
 @AutoConfiguration
 public class CorsConfigurationSourceAutoConfiguration {
